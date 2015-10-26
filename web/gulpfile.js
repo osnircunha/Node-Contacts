32608ed81c9a2c98157538d5a030ee51f42a1d17
osnircunha <osnircunha@gmail.com>
2015-10-26 01:16:19 -0200

    CSS minify

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,7 +1,11 @@
-var jshint = require('gulp-jshint');
 var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+
 var ngAnnotate = require('gulp-ng-annotate');
+
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var header = require('gulp-header');
@@ -10,10 +14,10 @@ var header = require('gulp-header');
 gulp.task('lint', function(){
     return gulp.src('scripts/**/*.js')
         .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+        .pipe(jshint.reporter());
  });
 
-gulp.task('minify', function () {
+gulp.task('minifyJs', function () {
     gulp.src('scripts/**/*.js')
         .pipe(concat('application.js'))
         .pipe(ngAnnotate())
@@ -23,8 +27,16 @@ gulp.task('minify', function () {
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('minifyCss', function () {
+    gulp.src('style/app.css')
+        .pipe(minifyCss())
+        .pipe(header('/*\n * Code analyzed by Gulp.\n*/\n'))
+        .pipe(rename({extname: '.min.css'}))
+        .pipe(gulp.dest('dist'));
+})
+
 gulp.task('watch', function () {
-    gulp.watch('scripts/**/*.js', ['lint', 'minify'])
+    gulp.watch('scripts/**/*.js', ['lint', 'minifyJs', 'minifyCss'])
 });
 
-gulp.task('default', ['lint', 'minify', 'watch']);
+gulp.task('default', ['lint', 'minifyJs', 'minifyCss', 'watch']);
